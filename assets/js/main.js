031f24099d0cedee67d800404f8dabea45c6898e
Vera Leitao <veraleitao.dev@gmail.com>
2020-08-30 12:11:27 +0000

    fixed bug when changing questions

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,10 +117,10 @@ resultArray;
 
 //--DOCUMENT.READY FUNCTION-----------------------------------
 $(document).ready(function () {
-
   //--Test Button Clicked from home page to open modal---------
+
   $("#myBtn").click(function () {
-    console.log("button was clicked, it works!");
+    console.log("home button was clicked, it works!");
 
     $(".bg-modal").show();
     $("#nextBtn").hide();
@@ -131,24 +131,27 @@ $(document).ready(function () {
     $("#startBtn").show();
   });
 
+
   //--close button function to return to home page------------
   $(".close").click(function () {
-      console.log("button was clicked, it works!");
-      $(".bg-modal").hide();
+    console.log("close button was clicked, it works!");
+    $(".bg-modal").hide();
 
-      function resetTest () {
-        i = 0;
-        resultArray = [];
-      };
-      resetTest();
-    });
+    function resetTest() {
+      i = 0;
+      resultArray = [];
+    }
+    resetTest();
+  });
 
   //--On clicking Start button (function)----------------------
   $("#startBtn").click(function () {
-    console.log("button was clicked, it works!");
+    console.log("start button was clicked, it works!");
+    
+    console.log("i = ", i);
 
     //On clicking Start, display the first question and show and disable the next button
-    
+
     $("#rulesContainer").hide();
     $("#questionContainer").show();
     $("#startBtn").hide();
@@ -157,50 +160,8 @@ $(document).ready(function () {
     $("#inputGroupSelect01").val("");
 
     disableNextBtn();
-
-    document.getElementById(
-      "myCompetencyType"
-    ).innerHTML = currentCompetencyType;
-    document.getElementById("myQuestion").innerHTML =
-      currentQuestionNumber + ".  " + currentQuestion;
-  });
-
-  //---------On clicking Next button function----------------
-  //---------------------------------------------------------
-  $("#nextBtn").click(function () {
-    console.log("button clicked, it works!");
-    disableNextBtn();
     disableResultsBtn();
 
-
-    //On clicking Next, display the next question
-    if(i < 17) {
-      i++;
-      console.log(i);
-      console.log(resultArrayLength);
-
-        $("#testForm").show();
-        $("#submitAlert").hide();
-        $("#inputGroupSelect01").val("");
-
-    } if (i >= 17) {
-        console.log("last question");
-
-        $("#nextBtn").hide();
-        $("#resultsBtn").show();
-
-    }
-        
-    
-    //local variables
-    var currentCompetencyType = questions[i].competence;
-    var currentQuestionNumber = questions[i].questionNumber;
-    var currentQuestion = questions[i].question;
-    console.log(currentCompetencyType);
-    console.log(currentQuestionNumber);
-    console.log(currentQuestion);
-
-    //change question every time next button is pressed
     document.getElementById(
       "myCompetencyType"
     ).innerHTML = currentCompetencyType;
@@ -208,32 +169,96 @@ $(document).ready(function () {
       currentQuestionNumber + ".  " + currentQuestion;
   });
 
-  //when a value is selected th
+  //when a value is selected on #testForm
   $("select").change(function () {
     console.log("a value has been selected");
 
     var valSelected = $("#inputGroupSelect01").val();
     valSelected;
-
-    resultArray.push(valSelected);
     console.log(valSelected);
+
+    resultArray.push(valSelected); //selected values from each question are pushed onto an array
     console.log(resultArray);
+
+    i++; //added 1 to the value of i which is going to select next question 
+    console.log("i = ", i);
 
     $("#testForm").hide();
     $("#submitAlert").show();
     $("#nextBtn").removeAttr("disabled");
     $("#resultsBtn").removeAttr("disabled");
+    
   });
+
+  //---------On clicking Next button function----------------
+  //---------------------------------------------------------
+  $("#nextBtn").click(function () {
+    console.log("next button clicked, it works!");
+
+    
+    var currentCompetencyType = questions[i].competence;
+    var currentQuestionNumber = questions[i].questionNumber;
+    var currentQuestion = questions[i].question;
+
+    console.log(currentCompetencyType);
+    console.log(currentQuestionNumber);
+    console.log(currentQuestion);
+    disableNextBtn();
+    disableResultsBtn();
+
+    /*On clicking Next, display the next question
+    with next button or results button (on last question)*/
+    if (i < 17) {
+    
+      console.log("the if condition was fired; i = ", i);
+      //console.log(resultArrayLength);
+
+
+      $("#testForm").show();
+      $("#submitAlert").hide();
+      $("#inputGroupSelect01").val("");
+
+      document.getElementById(
+        "myCompetencyType"
+      ).innerHTML = currentCompetencyType;
+      document.getElementById("myQuestion").innerHTML =
+        currentQuestionNumber + ".  " + currentQuestion;
+    };
+     if (i == 17) {
+      console.log("This is the very last question...");
+
+      $("#nextBtn").hide();
+      $("#resultsBtn").show();
+      $("#testForm").show();
+      $("#submitAlert").hide();
+      $("#inputGroupSelect01").val("");
+
+      document.getElementById(
+        "myCompetencyType"
+      ).innerHTML = currentCompetencyType;
+      document.getElementById("myQuestion").innerHTML =
+        currentQuestionNumber + ".  " + currentQuestion;
+    };
+
+    //change question every time next button is pressed
+  });
+
+  //$("#resultsBtn").click(function () {});
 
   //---------------------next button disabled function
   function disableNextBtn() {
     $("#nextBtn").attr("disabled", "disabled");
   };
 
+  //---------------------results button disabled function
   function disableResultsBtn() {
     $("#resultsBtn").attr("disabled", "disabled");
   };
 
+
+  console.log(currentCompetencyType);
+  console.log(currentQuestionNumber);
+  console.log(currentQuestion);
 
   disableNextBtn();
   disableResultsBtn();
