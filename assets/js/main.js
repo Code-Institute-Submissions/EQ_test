33f2fec473f53ae022f2ae5ab0339e84d3321f65
Vera Leitao <veraleitao.dev@gmail.com>
2020-09-10 08:53:42 +0000

    replaced dropdown form for answers for radio buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,135 +1,134 @@
-
 $(document).ready(function () {
-var questions = [
-  {
-    competence: "Personal competence",
-    questionNumber: "1",
-    questionP1: "Emotional self-awareness: ",
-    questionP2:
-      "I am aware of my own emotions and know their impact on my actions and decisions.",
-  },
-  {
-    competence: "Personal competence",
-    questionNumber: "2",
-    questionP1: "Accurate self-assessment: ",
-    questionP2:
-      "I know my own limits and possibilities, without overestimating or underestimating myself.",
-  },
-  {
-    competence: "Personal competence",
-    questionNumber: "3",
-    questionP1: "Self-confidence: ",
-    questionP2:
-      "I have a solid sense of my own value, capabilities and potential.",
-  },
-  {
-    competence: "Personal competence",
-    questionNumber: "4",
-    questionP1: "Emotional self-control: ",
-    questionP2:
-      "I maintain emotions and destructive impulses under control, even in times of stress.",
-  },
-  {
-    competence: "Personal competence",
-    questionNumber: "5",
-    questionP1: "Overcoming: ",
-    questionP2:
-      "I have motivation to improve performance and to meet my high standards.",
-  },
-  {
-    competence: "Personal competence",
-    questionNumber: "6",
-    questionP1: "Initiative: ",
-    questionP2: "I will always be ready to act and take opportunities.",
-  },
-  {
-    competence: "Personal competence",
-    questionNumber: "7",
-    questionP1: "Transparency: ",
-    questionP2: "I am honest and righteous, worthy of trust.",
-  },
-  {
-    competence: "Personal competence",
-    questionNumber: "8",
-    questionP1: "Adaptability: ",
-    questionP2:
-      "I have flexibility in adapting to people with a different style. I can adapt to difficult situations and different ways of thinking.",
-  },
-  {
-    competence: "Personal competence",
-    questionNumber: "9",
-    questionP1: "Optimism: ",
-    questionP2: "I see the bright side of events in any situation.",
-  },
-  {
-    competence: "Social competence",
-    questionNumber: "1",
-    questionP1: "Empathy: ",
-    questionP2:
-      "I Understand other people's emotions and their points of view. I take an active interest in people's concerns.",
-  },
-  {
-    competence: "Social competence",
-    questionNumber: "2",
-    questionP1: "Organizational awareness: ",
-    questionP2:
-      "I can identify and understand trends and influencial networks. I understand the politics of organizational level.",
-  },
-  {
-    competence: "Social competence",
-    questionNumber: "3",
-    questionP1: "Service: ",
-    questionP2:
-      "Identify and satisfy the needs of colleagues and customers. I support and help them improve their performance and achieve their goals.",
-  },
-  {
-    competence: "Social competence",
-    questionNumber: "4",
-    questionP1: "Inspirational leadership: ",
-    questionP2:
-      "I guide and motivate, with a thought-provoking vision, leading people to achieve mutual goals.",
-  },
-  {
-    competence: "Social competence",
-    questionNumber: "5",
-    questionP1: "Influence: ",
-    questionP2: "I have the ability to persuade and influence people.",
-  },
-  {
-    competence: "Social competence",
-    questionNumber: "6",
-    questionP1: "Development of others: ",
-    questionP2: "I nurture others habilities through feedback and guidance.",
-  },
-  {
-    competence: "Social competence",
-    questionNumber: "7",
-    questionP1: "Promoting change: ",
-    questionP2:
-      "I start make change happen, leading people in new directions.",
-  },
-  {
-    competence: "Social competence",
-    questionNumber: "8",
-    questionP1: "Conflict management: ",
-    questionP2:
-      "I resolve differences between people, leading them to mutual integration and acceptance.",
-  },
-  {
-    competence: "Social competence",
-    questionNumber: "9",
-    questionP1: "Teamwork: ",
-    questionP2: "I achieve high performance, collaboration and team work.",
-  },
-];
+  const questions = [
+    {
+      competence: "Personal competence",
+      questionNumber: "1",
+      questionP1: "Emotional self-awareness: ",
+      questionP2:
+        "I am aware of my own emotions and know their impact on my actions and decisions.",
+    },
+    {
+      competence: "Personal competence",
+      questionNumber: "2",
+      questionP1: "Accurate self-assessment: ",
+      questionP2:
+        "I know my own limits and possibilities, without overestimating or underestimating myself.",
+    },
+    {
+      competence: "Personal competence",
+      questionNumber: "3",
+      questionP1: "Self-confidence: ",
+      questionP2:
+        "I have a solid sense of my own value, capabilities and potential.",
+    },
+    {
+      competence: "Personal competence",
+      questionNumber: "4",
+      questionP1: "Emotional self-control: ",
+      questionP2:
+        "I maintain emotions and destructive impulses under control, even in times of stress.",
+    },
+    {
+      competence: "Personal competence",
+      questionNumber: "5",
+      questionP1: "Overcoming: ",
+      questionP2:
+        "I have motivation to improve performance and to meet my high standards.",
+    },
+    {
+      competence: "Personal competence",
+      questionNumber: "6",
+      questionP1: "Initiative: ",
+      questionP2: "I will always be ready to act and take opportunities.",
+    },
+    {
+      competence: "Personal competence",
+      questionNumber: "7",
+      questionP1: "Transparency: ",
+      questionP2: "I am honest and righteous, worthy of trust.",
+    },
+    {
+      competence: "Personal competence",
+      questionNumber: "8",
+      questionP1: "Adaptability: ",
+      questionP2:
+        "I have flexibility in adapting to people with a different style. I can adapt to difficult situations and different ways of thinking.",
+    },
+    {
+      competence: "Personal competence",
+      questionNumber: "9",
+      questionP1: "Optimism: ",
+      questionP2: "I see the bright side of events in any situation.",
+    },
+    {
+      competence: "Social competence",
+      questionNumber: "1",
+      questionP1: "Empathy: ",
+      questionP2:
+        "I Understand other people's emotions and their points of view. I take an active interest in people's concerns.",
+    },
+    {
+      competence: "Social competence",
+      questionNumber: "2",
+      questionP1: "Organizational awareness: ",
+      questionP2:
+        "I can identify and understand trends and influencial networks. I understand the politics of organizational level.",
+    },
+    {
+      competence: "Social competence",
+      questionNumber: "3",
+      questionP1: "Service: ",
+      questionP2:
+        "Identify and satisfy the needs of colleagues and customers. I support and help them improve their performance and achieve their goals.",
+    },
+    {
+      competence: "Social competence",
+      questionNumber: "4",
+      questionP1: "Inspirational leadership: ",
+      questionP2:
+        "I guide and motivate, with a thought-provoking vision, leading people to achieve mutual goals.",
+    },
+    {
+      competence: "Social competence",
+      questionNumber: "5",
+      questionP1: "Influence: ",
+      questionP2: "I have the ability to persuade and influence people.",
+    },
+    {
+      competence: "Social competence",
+      questionNumber: "6",
+      questionP1: "Development of others: ",
+      questionP2: "I nurture others habilities through feedback and guidance.",
+    },
+    {
+      competence: "Social competence",
+      questionNumber: "7",
+      questionP1: "Promoting change: ",
+      questionP2:
+        "I start make change happen, leading people in new directions.",
+    },
+    {
+      competence: "Social competence",
+      questionNumber: "8",
+      questionP1: "Conflict management: ",
+      questionP2:
+        "I resolve differences between people, leading them to mutual integration and acceptance.",
+    },
+    {
+      competence: "Social competence",
+      questionNumber: "9",
+      questionP1: "Teamwork: ",
+      questionP2: "I achieve high performance, collaboration and team work.",
+    },
+  ];
 
-var i = 0;
-var currentCompetencyType = questions[i].competence;
-var currentQuestionNumber = questions[i].questionNumber;
-var currentQuestionP1 = questions[i].questionP1;
-var currentQuestionP2 = questions[i].questionP2;
-var resultArray = [];
-resultArray;
+  var i = 0;
+  var currentCompetencyType = questions[i].competence;
+  var currentQuestionNumber = questions[i].questionNumber;
+  var currentQuestionP1 = questions[i].questionP1;
+  var currentQuestionP2 = questions[i].questionP2;
+  var resultArray = [];
+  resultArray;
 
   //--Test Button Clicked from home page to open modal---------
 
@@ -142,8 +141,6 @@ resultArray;
     $("#nextBtn").hide();
     $("#resultsBtn").hide();
     $("#questionContainer").hide();
-    
-    
   });
 
   //click "read more" button to expand the rules' text
@@ -176,7 +173,6 @@ resultArray;
     $("#startBtn").hide();
     $("#nextBtn").show();
     $("#testForm").show();
-    
 
     document.getElementById(
       "myCompetencyType"
@@ -188,145 +184,164 @@ resultArray;
     ).innerHTML = currentQuestionP2;
   });
 
-
   //---------On clicking Next button function----------------
   //---------------------------------------------------------
   /*On clicking Next, display the next question
                 with next button or results button (on last question)*/
   $("#nextBtn").click(function () {
     console.log("next button clicked, it works!");
-    var stringSelected = $("input:radio[name=likert]:checked").val()
-    var valSelected = parseInt(stringSelected);
+    var stringSelected = $("input:radio[name=likert]:checked").val(); // collects selected value
+    var valSelected = parseInt(stringSelected); // change value from string to number
     console.log("next button " + valSelected);
+    console.log("when next btn clicked i = " + i);
 
-
-    if (isNaN (valSelected)) {
+    if (isNaN(valSelected)) {
       alert("Ops! You need to select a value first.");
-      
     } else {
+      i++;
+      console.log("when else condition if fired i = " + i);
+      var currentCompetencyType = questions[i].competence;
+      var currentQuestionNumber = questions[i].questionNumber;
+      var currentQuestionP1 = questions[i].questionP1;
+      var currentQuestionP2 = questions[i].questionP2;
 
-    i++; //added 1 to the value of i which is going to select next question
-    console.log("i = ", i);
-    var currentCompetencyType = questions[i].competence;
-    var currentQuestionNumber = questions[i].questionNumber;
-    var currentQuestionP1 = questions[i].questionP1;
-    var currentQuestionP2 = questions[i].questionP2;
-    
-   
-    document.getElementById(
-          "myCompetencyType"
-        ).innerHTML = currentCompetencyType;
-        document.getElementById("myQuestion").innerHTML =
-          currentQuestionNumber + ".  " + currentQuestionP1;
-        document.getElementById(
-          "descriptionMyQuestion"
-        ).innerHTML = currentQuestionP2;
+      document.getElementById(
+        "myCompetencyType"
+      ).innerHTML = currentCompetencyType;
+      document.getElementById("myQuestion").innerHTML =
+        currentQuestionNumber + ".  " + currentQuestionP1;
+      document.getElementById(
+        "descriptionMyQuestion"
+      ).innerHTML = currentQuestionP2;
 
-     
       if (i < 17) {
-        console.log("the if i<17 condition was fired; i = ", i);
+        console.log("the if i<18 condition was fired; i = ", i);
 
-        resultArray.push(valSelected); //selected values from each question are pushed onto an array
+        resultArray.push(valSelected); //selected values from each question are pushed onto resultArray
         console.log(resultArray);
         $("#testForm").show();
-        
       }
       if (i == 17) {
         console.log("This is the very last question...");
-        
+
         resultArray.push(valSelected); //selected values from each question are pushed onto an array
         console.log(resultArray);
         $("#nextBtn").hide();
         $("#resultsBtn").show();
-          
       }
-    } 
-    $('input[name="likert"]').prop('checked', false);
-    
+    }
+    $('input[name="likert"]').prop("checked", false);
   });
 
   //display result;
   $("#resultsBtn").click(function () {
+    var stringSelected = $("input:radio[name=likert]:checked").val(); // collects selected value
+    var valSelected = parseInt(stringSelected); // change value from string to number
+    console.log("results button added " + valSelected);
+    console.log("when results btn clicked i = " + i);
+    resultArray.push(valSelected); //selected values from each question are pushed onto an array
+    console.log(resultArray);
+
     $(".bg-modal").hide();
     $("#resultsBtn").hide();
     $("#questionContainer").hide();
 
     $("#resultsContainer").show();
 
+
+    //to obtain the number of occurrences a number appear in one array
+    function getOccurrence(array, value) {
+      return array.filter((v) => v === value).length;
+    }
+
+    var stronglyAgreeOcc = getOccurrence(resultArray, 5);
+    var agreeOcc = getOccurrence(resultArray, 4);
+    var neutralOcc = getOccurrence(resultArray, 3);
+    var disagreeOcc = getOccurrence(resultArray, 2);
+    var stronglyDisagreeOcc = getOccurrence(resultArray, 1);
+    console.log("Strongly Agree occurs " + stronglyAgreeOcc + "times");
+    console.log(" Agree occurs " + agreeOcc + "times");
+    console.log("Neutral occurs " + neutralOcc + "times");
+    console.log("Disagree occurs " + disagreeOcc + "times");
+    console.log("Strongly Disagree occurs " + stronglyDisagreeOcc + "times");
+
+     //to calculate percentage of occurrences
+    function percentage(num, total) {
+      return (num / total) * 100;
+    }
+
+    var stronglyAgreeOccPercentage = percentage(stronglyAgreeOcc, 18);
+    var agreeOccPercentage = percentage(agreeOcc, 18);
+    var neutralOccPercentage = percentage(neutralOcc, 18);
+    var disagreeOccPercentage = percentage(disagreeOcc, 18);
+    var stronglyDisagreeOccPercentage = percentage(stronglyDisagreeOcc, 18);
+
+    // Radialize the colors
+    Highcharts.setOptions({
+      colors: Highcharts.map(Highcharts.getOptions().colors, function (color) {
+        return {
+          radialGradient: {
+            cx: 0.5,
+            cy: 0.3,
+            r: 0.7,
+          },
+          stops: [
+            [0, color],
+            [1, Highcharts.color(color).brighten(-0.3).get("rgb")], // darken
+          ],
+        };
+      }),
+    });
+
+    // Build the chart
     var resultsChart = Highcharts.chart("container", {
       chart: {
-        type: "column",
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: "pie",
       },
       title: {
-        text: "Your Current EQ",
-      },
-      subtitle: {
-        text: "",
-      },
-      xAxis: {
-        type: "category",
-        labels: {
-          rotation: -45,
-          style: {
-            fontSize: "13px",
-            fontFamily: "Verdana, sans-serif",
-          },
-        },
-      },
-      yAxis: {
-        min: 0,
-        title: {
-          text: "Answer values 1-10",
-        },
-      },
-      legend: {
-        enabled: false,
+        text: "Frequency of Answers",
       },
       tooltip: {
-        pointFormat: "",
+        pointFormat: "{series.name}: <b>{point.percentage:.1f}%</b>",
+      },
+      accessibility: {
+        point: {
+          valueSuffix: "%",
+        },
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: "pointer",
+          dataLabels: {
+            enabled: true,
+            format: "<b>{point.name}</b>: {point.percentage:.1f} %",
+            connectorColor: "silver",
+          },
+        },
       },
       series: [
         {
-          name: "Questions",
+          name: "Share",
           data: [
-            ["Emotional self-awareness", resultArray[0]],
-            ["Accurate self-assessment", resultArray[1]],
-            ["Self-confidence", resultArray[2]],
-            ["Emotional self-control", resultArray[3]],
-            ["Overcoming", resultArray[4]],
-            ["Initiative", resultArray[5]],
-            ["Transparency", resultArray[6]],
-            ["adaptability", resultArray[7]],
-            ["Optimism", resultArray[8]],
-            ["Empathy", resultArray[9]],
-            ["Organizational awareness", resultArray[10]],
-            ["Service", resultArray[11]],
-            ["Inspirational leadership", resultArray[12]],
-            ["Influence", resultArray[13]],
-            ["Development of others", resultArray[14]],
-            ["Promoting Change", resultArray[15]],
-            ["Conflict management", resultArray[16]],
-            ["Teamwork", resultArray[17]],
+            { name: "Strongly Agree", y: stronglyAgreeOccPercentage },
+            { name: "Agree", y: agreeOccPercentage },
+            { name: "Neutral", y: neutralOccPercentage },
+            { name: "Disagree", y: disagreeOccPercentage },
+            { name: "Strongly Disagree", y: stronglyDisagreeOccPercentage },
           ],
-          dataLabels: {
-            enabled: true,
-            rotation: -90,
-            color: "#FFFFFF",
-            align: "right",
-            format: "{point.y:.1f}", // one decimal
-            y: 10, // 10 pixels down from the top
-            style: {
-              fontSize: "13px",
-              fontFamily: "Monserrat, sans-serif",
-            },
-          },
         },
       ],
     });
     resultsChart;
 
-    document.getElementById("myCompetencyType").innerHTML = resultsChart;
+    
 
+    
+    /*----MORE CALCULATIONS-------*/
     //var resultArray = parseInt(resultArray);
     console.log("The resultArray is ", resultArray);
 
@@ -344,32 +359,26 @@ resultArray;
     var intNumber = Math.trunc(average);
     intNumber;
 
-    /*--To print the result change average Score 
-    from float number to whole number and display then
-    display result text accordingly--*/
+    
+    /*-------To print the result according to score (score number is hidden as we used a likert scale--------------------------*/
     console.log("Average is " + average);
-    document.getElementById("averageScore").innerHTML =
-      "Your average score is " + intNumber + ".";
-    if (intNumber >= 1 && intNumber <= 4) {
-      console.log("intNumber >= 1 && intNumber <= 4 condition was fired");
-      $("#result1-4").show();
+    
+    if (intNumber <= 2) {
+      console.log("intNumber <= 2 condition was fired");
+      $("#result1-2").show();
     }
-    if (intNumber >= 5 && intNumber <= 7) {
-      console.log("intNumber >= 5 && intNumber <= 7 condition was fired");
-      $("#result5-7").show();
+    if (intNumber == 3) {
+      console.log("intNumber == 3 condition was fired");
+      $("#result3").show();
     }
-    if (intNumber >= 8 && intNumber <= 10) {
-      console.log("intNumber >= 8 && intNumber <= 10 condition was fired");
-      $("#result8-10").show();
+    if (intNumber >= 4) {
+      console.log("iintNumber >= 4 condition was fired");
+      $("#result4-5").show();
     }
   });
-
-  
 
   console.log(currentCompetencyType);
   console.log(currentQuestionNumber);
   console.log(currentQuestionP1);
   console.log(currentQuestionP2);
-
- 
 });
